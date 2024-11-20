778c820f3ec3105ff2192f219918d7daa5266b97
gravitysword <147303481+gravitysword@users.noreply.github.com>
2024-11-20 12:53:47 +0800

    Update export.py

--- spacex/utils/export.py
+++ spacex/utils/export.py
@@ -56,27 +56,21 @@ def export_hot():
         print(f"发生错误: {e}")
 
 
-def zip():
+def zip_folder(folder_to_zip, output_zip_file):
     # 定义要压缩的文件夹路径
-    folders_to_zip = [
-        '../live/cache/',
-        '../hot/cache/'
-    ]
-    output_zip_file = 'export.zip'
-
-    temp_dir = 'temp/'
+    folder_name = os.path.basename(folder_to_zip)
+    temp_dir = 'temp/hot'
     os.makedirs(temp_dir, exist_ok=True)
 
-    for folder in folders_to_zip:
-        folder_name = os.path.basename(folder)
-        target_dir = os.path.join(temp_dir, folder_name)
+    # 目标目录
+    target_dir = os.path.join(temp_dir, folder_name)
 
-        # 检查并删除已存在的目标目录
-        if os.path.exists(target_dir):
-            shutil.rmtree(target_dir)
+    # 检查并删除已存在的目标目录
+    if os.path.exists(target_dir):
+        shutil.rmtree(target_dir)
 
-        # 复制文件夹
-        shutil.copytree(folder, target_dir)
+    # 复制文件夹
+    shutil.copytree(folder_to_zip, target_dir)
 
     # 压缩临时目录
     shutil.make_archive(output_zip_file.replace('.zip', ''), 'zip', temp_dir)
@@ -86,6 +80,39 @@ def zip():
 
     print(f'文件夹已成功压缩为 {output_zip_file}')
 
+def zip_files(file_list, output_zip):
+    """
+    将多个文件压缩到一个ZIP文件中。
+
+    :param file_list: 要压缩的文件列表
+    :param output_zip: 输出的ZIP文件路径（不带 .zip 后缀）
+    """
+    # 检查文件是否存在
+    existing_files = [file for file in file_list if os.path.exists(file)]
+    if not existing_files:
+        print("没有找到任何文件，无法创建ZIP文件。")
+        return
+
+    for file in file_list:
+        if not os.path.exists(file):
+            print(f"警告：文件 {file} 不存在，跳过。")
+
+    # 创建一个临时目录来存放文件
+    temp_dir = 'temp_zip_dir'
+    os.makedirs(temp_dir, exist_ok=True)
+
+    try:
+        # 复制文件到临时目录
+        for file in existing_files:
+            shutil.copy(file, temp_dir)
+
+        # 创建ZIP文件
+        shutil.make_archive(output_zip, 'zip', temp_dir)
+    finally:
+        # 删除临时目录
+        shutil.rmtree(temp_dir)
+
+
 if __name__ == '__main__':
     export_hot()
     export_live()
@@ -93,4 +120,6 @@ if __name__ == '__main__':
         a = f.read()
     with open('../live/cache/rooms.json', 'w', encoding='utf-8') as f:
         f.write(a)
-    zip()
+    zip_folder('../live/cache', 'live.zip')
+    zip_folder('../hot/cache', 'hot.zip')
+    zip_files(['live.zip','hot.zip'],'export')
